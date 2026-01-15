742a2bf2b863be1f32d599381130bf18483bc2c8
Lukas Ebeling <lukas.ebeling@desy.de>
2026-01-15 14:05:47 +0100

    new training

--- TOPO_HHbbWW_1mu_v2/scaling.h
+++ TOPO_HHbbWW_1mu_v2/scaling.h
@@ -1,7 +1,7 @@
 #ifndef __ADT_SCALES_H
 #define __ADT_SCALES_H
 
-namespace hls4ml_topo_HHbbWW_1mu_v2{
+namespace standardscaler{
 
 int norm[21] = {128,32,128,256,8,4,32,64,2,64,64,64,64,64,64,32,32,64,32,32,64};
 int bias[21] = {43,3,0,52,2,0,0,8,0,60,1,64,37,1,51,22,1,36,12,0,23};
