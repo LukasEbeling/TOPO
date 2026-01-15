d304a4a972df1d9772d7ab4505381a55b89ac676
Lukas Ebeling <lukas.ebeling@desy.de>
2026-01-15 16:20:14 +0100

    version 3 (no sigmoid)

---
+++ TOPO_HHbbWW_1mu_v3/NN/weights/b11.h
@@ -0,0 +1,16 @@
+//Numpy array shape [1]
+//Min -1.406250000000
+//Max -1.406250000000
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifdef __HLS4ML_LOAD_TXT_WEIGHTS__
+bias11_t b11[1];
+#else
+bias11_t b11[1] = {-1.406250000};
+
+#endif
+
+#endif
